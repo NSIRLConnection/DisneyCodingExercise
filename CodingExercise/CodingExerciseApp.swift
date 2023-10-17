f8bb785147dcf7296323519a5ed38dfb1af4268b
Michael Yau <53442828+miyau-discovery@users.noreply.github.com>
2023-10-17 13:48:15 -0400

    Initial Commit

---
+++ CodingExercise/CodingExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingExerciseApp.swift
+//  CodingExercise
+//
+//  Created by Michael Yau on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct CodingExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
